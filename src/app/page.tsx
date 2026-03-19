7cab24ef77a4bb0dc1c3bdd8155633a14335bfa3
Witek G <witold@grzegorczyk.it>
2026-03-19 14:04:09 +0100

    Write static part of the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
 export default function Home() {
-  return <main>Hello World!</main>;
+  return (
+    <main className="flex h-full w-full flex-col items-center justify-center gap-y-8">
+      <section>
+        <h1 className="text-4xl font-bold tracking-wider">Todo List</h1>
+      </section>
+      <section className="">
+        <ul className="border-t border-t-zinc-300">
+          <li className="flex w-full items-center justify-center gap-5 border-b border-b-zinc-300 p-3">
+            <input type="checkbox" />
+            <p>Apples</p>
+          </li>
+          <li className="flex w-full items-center justify-center gap-5 border-b border-b-zinc-300 p-3">
+            <input type="checkbox" />
+            <p>Apples</p>
+          </li>
+          <li className="flex w-full items-center justify-center gap-5 border-b border-b-zinc-300 p-3">
+            <input type="checkbox" />
+            <p>Apples</p>
+          </li>
+        </ul>
+      </section>
+    </main>
+  );
 }
