757b2c1dc67eafe65af0c586780ea89a8c1c9b05
Witek G <witold@grzegorczyk.it>
2026-03-20 13:22:15 +0100

    Handle Adding Items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,17 @@ const testInitialItems: TodoItemT[] = [
 
 export default function Home() {
   const [items, setItems] = useState(testInitialItems);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleAddItem = (name: string) => {
+    const newItem = {
+      name,
+      checked: false,
+      id: items.length + 1,
+    };
+    setItems((prev) => [...prev, newItem]);
+    setInputValue("");
+  };
 
   const handleToggleItem = (id: number) => {
     setItems((prev) =>
@@ -49,11 +60,23 @@ export default function Home() {
           ))}
           <li className="flex w-full items-center gap-5 border-b border-b-zinc-300 p-3 transition hover:bg-zinc-50">
             <Checkbox checked={false} />
-            <input
-              type="text"
-              className="max-h-6 border-b border-b-zinc-200 transition-all outline-none hover:border-b-zinc-400 focus:border-b-zinc-400 focus:ring-0 focus:ring-offset-0"
-              placeholder="Add a new item..."
-            />
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleAddItem(inputValue);
+              }}
+            >
+              <input
+                type="text"
+                value={inputValue}
+                onChange={(e) => {
+                  e.preventDefault();
+                  setInputValue(e.target.value);
+                }}
+                className="max-h-6 border-b border-b-zinc-200 transition-all outline-none hover:border-b-zinc-400 focus:border-b-zinc-400 focus:ring-0 focus:ring-offset-0"
+                placeholder="Add a new item..."
+              />
+            </form>
           </li>
         </ul>
       </section>
